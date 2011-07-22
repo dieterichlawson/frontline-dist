9a0a868b1fe97551ef5a4f2c0b162986bbf1e8a0
Dieterich Lawson <dieterich.lawson@gmail.com>
2011-07-22 12:08:57 -0500

    Removing unnecessary plugins in PluginLoadingTest.java

--- src/test/java/net/frontlinesms/PluginLoadingTest.java
+++ src/test/java/net/frontlinesms/PluginLoadingTest.java
@@ -3,20 +3,15 @@
  */
 package net.frontlinesms;
 
-import org.springframework.context.ApplicationContext;
-
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import net.frontlinesms.events.EventBus;
 import net.frontlinesms.junit.BaseTestCase;
 import net.frontlinesms.plugins.PluginController;
 import net.frontlinesms.plugins.PluginInitialisationException;
 import net.frontlinesms.plugins.forms.FormsPluginController;
-import net.frontlinesms.plugins.httptrigger.HttpTriggerPluginController;
-import net.frontlinesms.plugins.reminders.RemindersPluginController;
-import net.frontlinesms.plugins.reminders.data.repository.ReminderDao;
-import net.frontlinesms.plugins.translation.TranslationPluginController;
 
-import static org.mockito.Mockito.*;
+import org.springframework.context.ApplicationContext;
 
 
 /**
@@ -32,10 +27,7 @@ public class PluginLoadingTest extends BaseTestCase {
 		ApplicationContext applicationContext = getMockApplicationContext();
 
 		Class<?>[] controllerClasses = new Class<?>[] {
-			TranslationPluginController.class,
 			FormsPluginController.class,
-			RemindersPluginController.class,
-			HttpTriggerPluginController.class,
 		};
 		
 		for(Class<?> c : controllerClasses) {
@@ -50,18 +42,11 @@ public class PluginLoadingTest extends BaseTestCase {
 		EventBus eventBus = mock(EventBus.class);
 		when(c.getBean("eventBus")).thenReturn(eventBus);
 		
-		doRemindersSetup(c);
 		return c;
-	}
-
-	private void doRemindersSetup(ApplicationContext c) {
-		ReminderDao dao = mock(ReminderDao.class);
-		when(c.getBean("reminderDao")).thenReturn(dao);
 	}
 
 	private FrontlineSMS getMockFrontlineController() {
 		FrontlineSMS f = mock(FrontlineSMS.class);
-		
 		return f;
 	}
 }
